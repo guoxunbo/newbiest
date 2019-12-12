937c2b2f705d9a38daacff3b5ad8c5dcbf606b07
guoxunbo <11603652@qq.com>
2019-12-12 11:49:44 +0800

    初步提交stdf

--- stdf/src/main/java/com/newbiest/common/stdf/read/StdfReader.java
+++ stdf/src/main/java/com/newbiest/common/stdf/read/StdfReader.java
@@ -1,6 +1,11 @@
 package com.newbiest.common.stdf.read;
 
+import com.newbiest.base.exception.ClientException;
+import com.newbiest.common.stdf.input.DataFile;
 import com.newbiest.common.stdf.input.DataFileReader;
+import com.newbiest.common.stdf.input.RecordUser;
+
+import java.io.InputStream;
 
 /**
  * Created by guoxunbo on 2019-11-28 18:30
@@ -8,4 +13,18 @@ import com.newbiest.common.stdf.input.DataFileReader;
 public class StdfReader implements DataFileReader {
 
 
+    @Override
+    public void addRecordUser(RecordUser paramRecordUser) {
+
+    }
+
+    @Override
+    public void readFile(InputStream paramInputStream, DataFile paramDataFile) throws ClientException {
+
+    }
+
+    @Override
+    public void abortReading() {
+
+    }
 }
