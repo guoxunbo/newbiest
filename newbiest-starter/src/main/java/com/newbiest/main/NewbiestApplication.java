3cd1c8fece66af8574d0d2d16913757438589e73
guoxunbo <11603652@qq.com>
2019-12-24 11:41:36 +0800

    跟随framework升级

--- newbiest-starter/src/main/java/com/newbiest/main/NewbiestApplication.java
+++ newbiest-starter/src/main/java/com/newbiest/main/NewbiestApplication.java
@@ -1,6 +1,7 @@
 package com.newbiest.main;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newbiest.base.msg.DefaultParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
