f0e2b8456f13a8b4fc325b4c78798dbc640c6b9a
guozhang.luo <1972685455@qq.com>
2020-02-13 13:42:16 +0800

    #169 预留栏位补全
    预留栏位补全备注信息

---
+++ newbiest-gc/src/main/resources/db/changelog/sqlfile/comments_0.0.9.sql
@@ -0,0 +1,23 @@
+/**
+ * GC使用到的reserved栏位做备注
+ */
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED1 is 'MES完成品的levelTwoCode';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED2 is 'MES完成品的waferId';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED3 is 'MES完成品的salesNote';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED4 is 'MES完成品的treasuryNote';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED5 is 'MES完成品的productionNote';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED6 is 'MES完成品的bondedProperty';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED7 is 'MES完成品的productCategory';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED8 is '中转箱号';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED9 is '装箱检验判定等级';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED10 is '装箱检验判定码';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED11 is '重测之后的工单号';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED12 is '单据号';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED13 is '接收的时候会记录默认仓库';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED14 is '记录StorageId';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED15 is '记录重测工单日期';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED16 is '记录备货的出货单LineRrn';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED17 is '记录备货的出货单号';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED18 is '备货备注';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED19 is '记录箱重量';
+comment on column MMS_MATERIAL_LOT_HIS.RESERVED20 is '记录多箱称重事务号';
