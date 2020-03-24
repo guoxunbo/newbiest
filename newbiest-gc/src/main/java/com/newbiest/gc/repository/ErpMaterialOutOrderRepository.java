018eb1d109115744d8427cfb85ee15d9bb28e1ff
guozhang.luo <1972685455@qq.com>
2020-03-24 11:30:45 +0800

    同步方法优化
    同步方法添加@Param参数

--- newbiest-gc/src/main/java/com/newbiest/gc/repository/ErpMaterialOutOrderRepository.java
+++ newbiest-gc/src/main/java/com/newbiest/gc/repository/ErpMaterialOutOrderRepository.java
@@ -5,6 +5,7 @@ import com.newbiest.base.repository.custom.IRepository;
 import com.newbiest.gc.model.ErpMaterialOutOrder;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,5 @@ public interface ErpMaterialOutOrderRepository extends IRepository<ErpMaterialOu
 
     @Query("update ErpMaterialOutOrder p set p.synStatus=:synStatus, p.errorMemo = :errorMemo where p.seq in (:seqList)")
     @Modifying
-    void updateSynStatusAndErrorMemoBySeq(String synStatus, String errorMemo, List<Long> seqList) throws ClientException;
+    void updateSynStatusAndErrorMemoBySeq(@Param("synStatus") String synStatus,@Param("errorMemo")  String errorMemo,@Param("seqList")  List<Long> seqList) throws ClientException;
 }
--- newbiest-gc/src/main/java/com/newbiest/gc/repository/ErpSoRepository.java
+++ newbiest-gc/src/main/java/com/newbiest/gc/repository/ErpSoRepository.java
@@ -5,6 +5,7 @@ import com.newbiest.base.repository.custom.IRepository;
 import com.newbiest.gc.model.ErpSo;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,5 @@ public interface ErpSoRepository extends IRepository<ErpSo, Long> {
 
     @Query("update ErpSo p set p.synStatus=:synStatus, p.errorMemo = :errorMemo where p.seq in (:seqList)")
     @Modifying
-    void updateSynStatusAndErrorMemoBySeq(String synStatus, String errorMemo, List<Long> seqList) throws ClientException;
+    void updateSynStatusAndErrorMemoBySeq(@Param("synStatus") String synStatus,@Param("errorMemo") String errorMemo,@Param("seqList") List<Long> seqList) throws ClientException;
 }
