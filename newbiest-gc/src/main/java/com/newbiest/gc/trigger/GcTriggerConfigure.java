5f4b4f26d70b092d5e52d7ebba400c9a3fe1ec42
guozhang.luo <1972685455@qq.com>
2020-09-21 14:22:30 +0800

    #325 产品二级代码管理
    取消mes产品二级代码信息的同步

--- newbiest-gc/src/main/java/com/newbiest/gc/trigger/GcTriggerConfigure.java
+++ newbiest-gc/src/main/java/com/newbiest/gc/trigger/GcTriggerConfigure.java
@@ -37,7 +37,7 @@ public class GcTriggerConfigure implements SchedulingConfigurer {
 
         scheduledTaskRegistrar.addCronTask(new AsyncProductThread(gcService), AsyncProductThread.CRON_EXPRESS);
         scheduledTaskRegistrar.addCronTask(new AsyncWaferTypeThread(gcService), AsyncWaferTypeThread.CRON_EXPRESS);
-        scheduledTaskRegistrar.addCronTask(new AsyncProductSubcodeThread(gcService), AsyncProductSubcodeThread.CRON_EXPRESS);
+//        scheduledTaskRegistrar.addCronTask(new AsyncProductSubcodeThread(gcService), AsyncProductSubcodeThread.CRON_EXPRESS);
         scheduledTaskRegistrar.addCronTask(new AsyncProductModelConversionThread(gcService), AsyncProductModelConversionThread.CRON_EXPRESS);
         scheduledTaskRegistrar.addCronTask(new AsyncProductRelationThread(gcService), AsyncProductRelationThread.CRON_EXPRESS);
         scheduledTaskRegistrar.addCronTask(new AsyncPoSupplierThread(gcService), AsyncPoSupplierThread.CRON_EXPRESS);
