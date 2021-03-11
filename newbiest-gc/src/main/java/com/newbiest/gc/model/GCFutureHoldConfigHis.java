4ba77b0faaafca7dd61268ce19452442f7ddf99c
dcyanzhi <49708273+dcyanzhi@users.noreply.github.com>
2021-03-11 17:13:05 +0800

    lot产线入库
    WLA/CP接收入库预约hold功能

---
+++ newbiest-gc/src/main/java/com/newbiest/gc/model/GCFutureHoldConfigHis.java
@@ -0,0 +1,29 @@
+package com.newbiest.gc.model;
+
+import com.newbiest.base.model.NBHis;
+import com.newbiest.base.model.NBUpdatable;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by wangxinqi
+ */
+@Entity
+@Table(name="GC_FUTURE_HOLD_CONFIG_HIS")
+@Data
+public class GCFutureHoldConfigHis extends NBHis {
+    /**
+     * LotId
+     */
+    @Column(name = "LOT_ID")
+    private String lotId;
+
+    /**
+     * HOLD原因
+     */
+    @Column(name="HOLD_REASON")
+    private String holdReason;
+}
