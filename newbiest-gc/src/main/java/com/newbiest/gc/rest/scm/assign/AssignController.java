fed0e80dfb7adc04fffc7229cdd8d18e1e6e0069
guoxunbo <11603652@qq.com>
2021-03-12 10:15:35 +0800

    scm标注用户不存在则自动创建用户

--- newbiest-gc/src/main/java/com/newbiest/gc/rest/scm/assign/AssignController.java
+++ newbiest-gc/src/main/java/com/newbiest/gc/rest/scm/assign/AssignController.java
@@ -5,11 +5,14 @@ import com.newbiest.base.utils.StringUtils;
 import com.newbiest.gc.service.GcService;
 import com.newbiest.gc.service.ScmService;
 import com.newbiest.msg.Request;
+import com.newbiest.security.model.NBUser;
+import com.newbiest.security.service.SecurityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +27,9 @@ public class AssignController {
     @Autowired
     ScmService scmService;
 
+    @Autowired
+    SecurityService securityService;
+
     @ApiOperation(value = "SCMReserved", notes = "SCM预留")
     @ApiImplicitParam(name="request", value="request", required = true, dataType = "AssignRequest")
     @RequestMapping(value = "/scmassign", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
@@ -34,6 +40,14 @@ public class AssignController {
         AssignResponseBody responseBody = new AssignResponseBody();
         AssignRequestBody requestBody = request.getBody();
 
+        String username = request.getHeader().getUsername();
+        NBUser nbUser = securityService.getUserByUsername(username);
+        if (nbUser == null) {
+            nbUser = new NBUser();
+            nbUser.setUsername(username);
+            nbUser.setDescription(username);
+            securityService.saveUser(nbUser);
+        }
         String actionType = requestBody.getActionType();
         if (AssignRequest.ACTION_TYPE_ASSIGN.equals(actionType)) {
             scmService.scmAssign(requestBody.getLotId(), requestBody.getVendor(), requestBody.getPoId(), requestBody.getMaterialType(), requestBody.getRemarks());
