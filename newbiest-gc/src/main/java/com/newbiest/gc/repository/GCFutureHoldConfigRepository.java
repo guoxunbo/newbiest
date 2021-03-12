f5c9fc6ad07f9c5840f538c8dc13394f1d3ea9b7
guozhang.luo <1972685455@qq.com>
2021-03-12 13:51:00 +0800

    #470 LOT产线入库
    1、产线入库继承来料时的仓库信息；
    2、修复产线入库时爆系统错误bug

--- newbiest-gc/src/main/java/com/newbiest/gc/repository/GCFutureHoldConfigRepository.java
+++ newbiest-gc/src/main/java/com/newbiest/gc/repository/GCFutureHoldConfigRepository.java
@@ -4,8 +4,11 @@ import com.newbiest.base.exception.ClientException;
 import com.newbiest.base.repository.custom.IRepository;
 import com.newbiest.gc.model.GCFutureHoldConfig;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GCFutureHoldConfigRepository extends IRepository<GCFutureHoldConfig,Long> {
+
     GCFutureHoldConfig findByLotId(@Param("lotId")String lotId) throws ClientException;
 
 }
