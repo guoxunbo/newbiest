b8135fca777d8c01ab5bee69f6d8030164e408b3
guozhang.luo <1972685455@qq.com>
2021-03-23 10:59:24 +0800

    #536 RW晶圆发料
    RW发料标签补打

---
+++ newbiest-gc/src/main/java/com/newbiest/gc/rest/rw/manager/RwMaterialLotRequestBody.java
@@ -0,0 +1,23 @@
+package com.newbiest.gc.rest.rw.manager;
+
+import com.newbiest.mms.model.MaterialLot;
+import com.newbiest.msg.RequestBody;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.util.List;
+
+
+@Data
+@ApiModel("具体请求操作信息")
+public class RwMaterialLotRequestBody extends RequestBody {
+	
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "操作类型", example = "Query/StockIn")
+	private String actionType;
+
+	@ApiModelProperty(value = "物料批次号")
+	private List<MaterialLot> materialLotList;
+
+}
