f9540bea8edcc4b8bd6cec510622a54f56d132ea
洋001H <73891005+Yoko001H@users.noreply.github.com>
2021-08-25 14:48:27 +0800

    原材料管理
    1.“原材料导入”卡控IRA的最小生产日期；
    2.“原材料备料”同一箱IRA通过最小生产日期筛选；
    3.“原材料发料“胶水有效期卡控。

--- newbiest-mm/src/main/java/com/newbiest/mms/model/MaterialLot.java
+++ newbiest-mm/src/main/java/com/newbiest/mms/model/MaterialLot.java
@@ -595,6 +595,14 @@ public class MaterialLot extends NBUpdatable implements StatusLifeCycle{
     private Date expDate;
 
     /**
+     * 最小原材料有效日期
+     */
+    @Column(name="EARLIER_EXP_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(timezone = GMT_PE,pattern = DateUtils.DEFAULT_DATETIME_PATTERN)
+    private Date earlierExpDate;
+
+    /**
      * 原材料发货日期
      */
     @Column(name="SHIPPING_DATE")
