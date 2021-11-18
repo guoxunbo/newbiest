4a93cb44261afde15c5241555435b3409915ea26
洋001H <73891005+Yoko001H@users.noreply.github.com>
2021-11-18 11:27:42 +0800

    保税仓库管理
    1.新添”来料导入“功能；
    2.新添”WLT/CP晶圆接收“功能；
    3.新添”晶圆入库位“功能；
    4.新添”LOT装箱“功能；
    5.新添”WLT/CP出货“功能；
    6.新添”WLT/CP其它出“功能。

---
+++ newbiest-gc/src/main/resources/db/changelog/sqlfile/insert_BondedWarehouseManager_to_nb_authority_0.0.1.sql
@@ -0,0 +1,2 @@
+insert into nb_authority (OBJECT_RRN, ACTIVE_FLAG, ORG_RRN, NAME, DESCRIPTION, PARENT_RRN, AUTHORITY_TYPE, AUTHORITY_CATEGORY, IMAGE, LABEL_EN, LABEL_ZH, LABEL_RES, SEQ_NO, TABLE_RRN, URL, PARAMETER1, PARAMETER2, PARAMETER3, PARAMETER4, PARAMETER5, MOBILE_FLAG)
+values (2021, 'Y', 0, 'BondedWarehouseManager', '保税仓库管理', null, 'M', 'MMS', 'icon-warehouse', 'Bonded Warehouse Manager', '保税仓库管理', null, 47, null, '/BondedWarehouse', null, null, null, null, null, 'N');
