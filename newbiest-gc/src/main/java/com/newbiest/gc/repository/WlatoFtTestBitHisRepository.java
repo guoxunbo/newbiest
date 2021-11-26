015b02aff89db9a96662cd0ff876c73486f625ba
dcyanzhi <49708273+dcyanzhi@users.noreply.github.com>
2021-11-26 16:48:27 +0800

    WLA入库记录测试码
    WLA入库记录测试码

---
+++ newbiest-gc/src/main/java/com/newbiest/gc/repository/WlatoFtTestBitHisRepository.java
@@ -0,0 +1,9 @@
+package com.newbiest.gc.repository;
+
+import com.newbiest.base.repository.custom.IRepository;
+import com.newbiest.gc.model.GcWlatoftTesebitHis;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WlatoFtTestBitHisRepository extends IRepository<GcWlatoftTesebitHis,Long> {
+}
